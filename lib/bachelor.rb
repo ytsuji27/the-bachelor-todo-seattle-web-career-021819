0e1449344b2d2cee5737458127d1cb8dd42bd450
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-21 04:06:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,7 @@ def get_average_age_for_season(data, season)
   ages = []
   data[season].each do |contestant_array|
     contestant_array.each do |attributes|
-      puts attributes
+      puts attributes["age"]
     end
   end
 #  ages.inject{ |sum, el| sum + el}
