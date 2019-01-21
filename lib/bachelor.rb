43c1be8b1bdaf564a8638fd033467bbce335fb91
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-21 03:36:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -20,7 +20,10 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant_array|
+    contestant_array.each do |attributes|
+      if attributes[""]
 end
 
 def get_occupation(data, hometown)
